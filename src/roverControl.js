e171f617dce0bd0af8169d06823dab8a394db0dc
djstephenson7 <dan_0502@hotmail.co.uk>
2019-03-27 12:32:13 +0000

    Rover takes multiple commands

--- src/roverControl.js
+++ src/roverControl.js
@@ -5,31 +5,30 @@ const roverOne = new Rover(1, 2, 'N');
 const roverTwo = new Rover(3, 3, 'E');
 
 function moveRover(rover, directionString) {
-  const hi = directionHandler(directionString);
-  console.log(hi);
-  const direction = rover.direction;
-  if (directionString === 'L') {
-    rover.direction = moves[direction].L;
-  } else if (directionString === 'R') {
-    rover.direction = moves[direction].R;
-  } else if (directionString === 'M') {
-    rover = moves[direction].move(rover.x, rover.y);
-    rover.direction = moves[direction].NONE;
-  }
+  const commands = directionString.split('');
 
-  console.log(`Rover status: ${rover.direction}, ${rover.x}, ${rover.y}`);
-  return rover.x, rover.y, rover.direction;
+  for (let i = 0; i < commands.length; i++) {
+    commands[i];
+
+    const direction = rover.direction;
+    if (commands[i] === 'L') {
+      rover.direction = moves[direction].L;
+    } else if (commands[i] === 'R') {
+      rover.direction = moves[direction].R;
+    } else if (commands[i] === 'M') {
+      rover = moves[direction].move(rover.x, rover.y);
+      rover.direction = moves[direction].NONE;
+    }
+  }
+  console.log(`Rover status: ${rover.x}, ${rover.y}, ${rover.direction}`);
+  return rover;
 }
 
 function directionHandler(commands) {
   return commands.split('');
 }
 
-directionHandler('LMLMLMLMM');
-
-moveRover(roverOne, 'M');
-// moveRover(roverOne, 'M');
-
+moveRover(roverOne, 'LMLMLM');
 
 function terrainHandler() {
   // 1. if latitude & longitude are < 0 or > 5, throw 'out of bounds' error
--- test/roverControl.test.js
+++ test/roverControl.test.js
@@ -4,9 +4,17 @@ const Rover = require('../src/rover');
 const roverOne = new Rover(1, 2, 'N');
 
 test('It changes from North to East', () => {
-  expect(moveRover(roverOne, 'R')).toEqual('E');
+  expect(moveRover(roverOne, 'R')).toEqual({ direction: 'E', x: 1, y: 2 });
 });
 
 test('It changes from East to North', () => {
-  expect(moveRover(roverOne, 'L')).toEqual('N');
+  expect(moveRover(roverOne, 'L')).toEqual({ direction: 'N', x: 1, y: 2 });
+});
+
+test('It moves forward in the direction it is facing', () => {
+  expect(moveRover(roverOne, 'M')).toEqual({ direction: 'N', x: 1, y: 3 });
+});
+
+test('It takes multiple commands as a string', () => {
+  expect(moveRover(roverOne, 'LMLMLM')).toEqual({ direction: 'E', x: 1, y: 1 });
 });
